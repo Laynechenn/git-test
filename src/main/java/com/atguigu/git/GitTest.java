b8c496da82443bc455e3231accb626f53863ec6e
Layue <936090389@qq>
2021-08-29 18:33:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-08-29 18:06
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
