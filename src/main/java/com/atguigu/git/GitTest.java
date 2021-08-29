0505191e1cf748807c444e8f3cd58a689deb75d9
Layue <936090389@qq>
2021-08-29 19:03:28 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,13 +1,11 @@
 package com.atguigu.git;
 
-/**
- * @author shkstart
- * @create 2021-08-29 18:06
- */
+
 public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git----");
     }
 
 }
